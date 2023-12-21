ea8d67506f3baf84ffa9ae65ab547d90726198a7
Omid Shabab <omidshababdev@gmail.com>
2023-12-21 03:34:08 -0500

    first commit

---
+++ src/components/header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.scss";
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+      <a>Awwards</a>
+      <a>Instagram</a>
+      <a>Dribbble</a>
+      <a>LinkedIn</a>
+    </div>
+  );
+}
---
+++ src/components/header/nav/index.jsx
@@ -0,0 +1,67 @@
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import styles from "./style.module.scss";
+import { menuSlide } from "../anim";
+import Link from "./link";
+import Footer from "./footer";
+import Curve from "./curve";
+import { useState } from "react";
+
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
+
+export default function index() {
+  const pathname = usePathname();
+  const [selectedIndicator, setSelectedIndicator] = useState(pathname);
+
+  return (
+    <motion.div
+      variants={menuSlide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className={styles.menu}
+    >
+      <div className={styles.body}>
+        <div
+          onMouseLeave={() => {
+            setSelectedIndicator(pathname);
+          }}
+          className={styles.nav}
+        >
+          <div className={styles.header}>
+            <p>Navigation</p>
+          </div>
+          {navItems.map((data, index) => {
+            return (
+              <Link
+                key={index}
+                data={{ ...data, index }}
+                isActive={selectedIndicator === data.href}
+                setSelectedIndicator={setSelectedIndicator}
+              ></Link>
+            );
+          })}
+        </div>
+        <Footer />
+      </div>
+      <Curve />
+    </motion.div>
+  );
+}
